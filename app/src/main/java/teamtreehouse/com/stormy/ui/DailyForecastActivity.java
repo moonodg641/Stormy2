a235e9cb95bd9f1b623e1f34ecfd0777187e5d2e
moonodg641 <moondog@hawaii.rr.com>
2016-10-21 00:05:21 -1000

    Initial commit

---
+++ app/src/main/java/teamtreehouse/com/stormy/ui/DailyForecastActivity.java
@@ -0,0 +1,37 @@
+package teamtreehouse.com.stormy.ui;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Parcelable;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Arrays;
+
+import teamtreehouse.com.stormy.R;
+import teamtreehouse.com.stormy.adapters.DayAdapter;
+import teamtreehouse.com.stormy.weather.Day;
+
+public class DailyForecastActivity extends ListActivity {
+
+    public static final String TAG = DailyForecastActivity.class.getSimpleName();
+    private Day[] mDays;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_forecast);
+
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
+        mDays = Arrays.copyOf(parcelables, parcelables.length, Day[].class);
+
+        Log.i(TAG, parcelables.length + "");
+
+        DayAdapter adapter = new DayAdapter(this, mDays);
+        setListAdapter(adapter);
+    }
+}
